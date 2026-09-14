31db54e76a9cd73d86d7b5f8975211412a289cc1
Oskarergod <osweb9073@usn.no>
2026-09-14 15:16:17 +0200

    eksempel2

---
+++ eksempel2.php
@@ -0,0 +1,12 @@
+<?php 
+$svar=$_POST ["svar"];
+if ($svar == 16 )
+    {
+        print("Riktig. 4 ganger 4 er 16");
+    }
+    else 
+    {
+        print("Feil. 4 ganger 4 er 16");
+    }
+    ?>
+    
