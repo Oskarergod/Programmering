1fe7f6628d9bd3b088f599c36a696c280c14da4a
Oskarergod <osweb9073@usn.no>
2026-09-14 15:29:35 +0200

    eksempel3

---
+++ eksempel3.php
@@ -0,0 +1,19 @@
+<?php
+$svar=@_Post["svar"]; 
+if (!$svar)
+    {
+        print ("Du har ikke svart på spørsmålet. <br />");  
+    }
+    else if ($svar == "M")
+{
+    Print ("Du har svart at du er en Mann. <br />");
+}
+else if ($svar == "K")
+    {
+        Print ("Du har svart at du er en Kvinne. <br />");
+    }
+    else
+        {
+            Print ("Du har ikke svart riktig. <br />");
+        }
+        ?>
